07737b23e0bef7149f20f1f1985f160b6acfeed4
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-05-09 13:11:15 -0300

    test: e2e check ins

---
+++ src/http/controllers/check-ins/create.test.ts
@@ -0,0 +1,37 @@
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Node Gym',
+        latitude: -25.4062761,
+        longitude: -49.2573674,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -25.4062761,
+        longitude: -49.2573674,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
