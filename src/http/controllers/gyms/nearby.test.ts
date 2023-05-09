22c07921e05b1a79090ed18a5defbbe99fcb93c6
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-05-09 12:49:25 -0300

    test: e2e gyms

---
+++ src/http/controllers/gyms/nearby.test.ts
@@ -0,0 +1,57 @@
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Nearby Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able list nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Near Gym',
+        description: 'Some description',
+        phone: '1234554321',
+        latitude: -25.4062761,
+        longitude: -49.2573674,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Far Gym',
+        description: 'Some description',
+        phone: '1234554321',
+        latitude: -25.6878523,
+        longitude: -49.167474,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -25.4062761,
+        longitude: -49.2573674,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Near Gym',
+      }),
+    ])
+  })
+})
---
+++ src/http/controllers/gyms/search.test.ts
@@ -0,0 +1,56 @@
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Search Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search gyms by title', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Node Gym',
+        description: 'Some description',
+        phone: '1234554321',
+        latitude: -25.4062761,
+        longitude: -49.2573674,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'React Gym',
+        description: 'Some description',
+        phone: '1234554321',
+        latitude: -25.4062761,
+        longitude: -49.2573674,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        q: 'Node',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Node Gym',
+      }),
+    ])
+  })
+})
